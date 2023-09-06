3b80bfdeb6c7844f7d88f489e9b6b8bbc472e9d2
Ilove-Heng <102523138+Ilove-Heng@users.noreply.github.com>
2023-09-06 15:19:39 +0700

    1 file modified, 2 added

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
-    <h1 class="text-red-500 text-3xl font-bold underline">Hello world!</h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <listItem
+      v-for="(item, index) in database"
+      :name="item.name"
+      :department="item.deparment"
+      :salary="item.salary"
+      :position="index"
+      v-on:deleteItem="deleteItem"
+    />
+  </main>
+</template>
+
+<script setup>
+let database = ref([
+  {
+    name: 'Johnny',
+    deparment: 'Marketing',
+    salary: 30000,
+  },
+  {
+    name: 'Sarah',
+    deparment: 'Engineering',
+    salary: 72000,
+  },
+  {
+    name: 'Terrance',
+    deparment: 'Executive',
+    salary: 125000,
+  },
+]);
+
+function deleteItem(position) {
+  database.value.splice(position, 1);
+}
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
